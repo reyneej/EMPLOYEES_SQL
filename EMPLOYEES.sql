79772638bb918aaa4e4eac56c6a73836dddb510e
reyneej <114208073+reyneej@users.noreply.github.com>
2025-07-18 00:44:18 +0700

    Add files via upload

---
+++ EMPLOYEES.sql
@@ -0,0 +1,45 @@
+select * from employeesDATA;			
+
+select emp_name, SUM(saleamount) AS total_sales FROM employeesDATA
+group by emp_name;
+
+SELECT 
+  STRFTIME('%Y-%m', Date) AS Month,
+  SUM(SaleAmount) AS TotalSales
+FROM employeesDATA
+GROUP BY Month
+ORDER BY Month;
+
+
+SELECT *
+FROM employeesDATA
+GROUP BY date
+ORDER BY date DESC
+LIMIT 30;
+
+SELECT emp_name,
+       Sum(saleamount) TOTAL_SALES
+FROM   employeesdata
+GROUP  BY emp_name
+HAVING TOTAL_SALES > (SELECT AVG(TOTAL_SALES) AS AVERAGE_SALES
+                      FROM   (SELECT Sum(saleamount) TOTAL_SALES
+                              FROM   employeesdata
+                              GROUP  BY emp_name));
+
+
+SELECT 
+    Emp_Name,
+    SUM(SaleAmount) AS total_sales
+FROM 
+    employeesDATA
+GROUP BY 
+    Emp_Name
+HAVING 
+    total_sales > (
+        SELECT AVG(total_sales_per_emp)
+        FROM (
+            SELECT SUM(SaleAmount) AS total_sales_per_emp
+            FROM employeesDATA
+            GROUP BY emp_name
+        )
+    );
